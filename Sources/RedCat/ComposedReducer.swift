f9048b99cb986d4189045b1e2a1f69d77775327e
Markus Pfeifer <markus.pfeifer@orquidea.de>
2021-05-10 01:37:03 +0200

    better compositions

--- Sources/RedCat/ComposedReducer.swift
+++ Sources/RedCat/ComposedReducer.swift
@@ -5,14 +5,29 @@
 //  Created by Markus Pfeifer on 06.05.21.
 //
 
-import Foundation
+import CasePaths
 
 
 public extension DependentReducer {
     
     @inlinable 
-    func compose<Next: DependentReducer>(with next: Next) -> ComposedReducer<Self, Next> {
+    func compose<Next: DependentReducer>(with next: Next) -> ComposedReducer<Self, Next> where Next.State == State {
         ComposedReducer(self, next)
+    }
+    
+    @inlinable
+    func compose<Next : DependentReducer>(with next: Next, property: WritableKeyPath<State, Next.State>) -> ComposedReducer<Self, LensReducer<State, Next>> {
+        compose(with: LensReducer(property, reducer: next))
+    }
+    
+    @inlinable
+    func compose<Next : DependentReducer>(with next: Next, aspect: CasePath<State, Next.State>) -> ComposedReducer<Self, PrismReducer<State, Next>> where State : Emptyable {
+        compose(with: PrismReducer(aspect, reducer: next))
+    }
+    
+    @inlinable
+    func compose<Next : DependentClassReducer>(with next: Next, aspect: CasePath<State, Next.State>) -> ComposedReducer<Self, ClassPrismReducer<State, Next>> {
+        compose(with: ClassPrismReducer(aspect, reducer: next))
     }
     
 }
