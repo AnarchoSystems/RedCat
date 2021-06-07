6932f4fdf46299cd3e5647b4cfa2734e1f374d4d
Markus Pfeifer <markus.pfeifer@orquidea.de>
2021-06-07 19:48:41 +0200

    moved implementation of composedreducer

--- Sources/RedCat/ReducerProtocol.swift
+++ Sources/RedCat/ReducerProtocol.swift
@@ -5,8 +5,7 @@
 //  Created by Markus Pfeifer on 05.05.21.
 //
 
-import Foundation
-
+import CasePaths
 
 
 public protocol ErasedReducer {
@@ -104,6 +103,55 @@ public extension ReducerProtocol {
                to state: inout State,
                environment: Dependencies) {
         apply(action, to: &state)
+    }
+    
+}
+
+
+public extension ErasedReducer {
+    
+    @inlinable
+    func compose<Next: ErasedReducer>(with next: Next) -> ComposedReducer<Self, Next> where Next.State == State {
+        ComposedReducer(self, next)
+    }
+    
+    @inlinable
+    func compose<Next : ErasedReducer>(with next: Next,
+                                       property: WritableKeyPath<State, Next.State>)
+    -> ComposedReducer<Self, DetailReducer<State, Next>> {
+        compose(with: DetailReducer(property, reducer: next))
+    }
+    
+    @inlinable
+    func compose<Next : ErasedReducer>(with next: Next,
+                                       aspect: CasePath<State, Next.State>)
+    -> ComposedReducer<Self, AspectReducer<State, Next>> where State : Releasable {
+        compose(with: AspectReducer(aspect, reducer: next))
+    }
+    
+}
+
+
+public struct ComposedReducer<R1 : ErasedReducer, R2 : ErasedReducer> : ErasedReducer where R1.State == R2.State {
+    
+    @usableFromInline
+    let re1 : R1
+    @usableFromInline
+    let re2 : R2
+    
+    @usableFromInline
+    init(_ re1: R1, _ re2: R2) {(self.re1, self.re2) = (re1, re2)}
+    
+    @inlinable
+    public func applyErased<Action : ActionProtocol>(_ action: Action, to state: inout R1.State, environment: Dependencies) {
+        re1.applyErased(action, to: &state, environment: environment)
+        re2.applyErased(action, to: &state, environment: environment)
+    }
+    
+    @inlinable
+    public func acceptsAction<Action : ActionProtocol>(_ action: Action) -> Bool {
+        re1.acceptsAction(action)
+            || re2.acceptsAction(action)
     }
     
 }
