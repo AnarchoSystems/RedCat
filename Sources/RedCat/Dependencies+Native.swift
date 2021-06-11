3e2e2c4d25d39a47a8301e0b2932604f3f51f8a7
Markus Pfeifer <markus.pfeifer@orquidea.de>
2021-06-12 01:22:14 +0200

    native dependencies values

---
+++ Sources/RedCat/Dependencies+Native.swift
@@ -0,0 +1,42 @@
+//
+//  Dependencies+Native.swift
+//  
+//
+//  Created by Markus Pfeifer on 12.06.21.
+//
+
+import Foundation
+
+
+public enum NativeValues : Dependency {
+    public static let defaultValue = ResolvedNativeValues()
+}
+
+
+public extension Dependencies {
+    var nativeValues : ResolvedNativeValues {
+        get {self[NativeValues.self]}
+        set {self[NativeValues.self] = newValue}
+    }
+}
+
+
+public final class ResolvedNativeValues {
+    
+    public lazy var debug : Bool = {
+        #if DEBUG
+        return true
+        #else
+        return false
+        #endif
+    }()
+    
+    public lazy var isSimulator : Bool = {
+        #if targetEnvironment(simulator)
+        return true
+        #else
+        return false
+        #endif
+    }()
+    
+}
